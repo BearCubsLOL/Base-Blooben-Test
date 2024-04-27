d9f30d660b20c70bf588ffc2504c1d09bb0553f1
BearCubsLOL <student.187827@worcesterschools.net>
2024-04-27 16:48:53 -0400

    Version 1.1

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import winsound
 def newForm():
     response = input("\nWould you like to submit a new form? \n").lower().strip()
 
-    if response == "yes":
+    if "yes" in response or response == "y":
         start()
-    elif response == "no":
+    elif "no" in response or response == "n":
         print("\nOk thank you, bye! Remember: we're only sometimes here to help. Hope we never see you again \n")
     else:
         print("\nCould not recognize response, please try again")
@@ -14,13 +14,13 @@ def start():
 
     blooben = input("\nDoes your question pass the base blooben test? \n").lower().strip()
 
-    if blooben == "yes":
+    if "yes" in blooben or "yeah" in blooben or blooben == "y":
         print("\nGood Job")
         newForm()
-    elif blooben == "no":
+    elif "no" in blooben or "nah" in blooben or blooben == "n":
         print("\nBad Job")
         newForm()
-    elif "what" in blooben or "idk" in blooben or "i don't know" in blooben or "i dont know" in blooben or "huh" in blooben or "huh?" in blooben:
+    elif "what" in blooben or "idk" in blooben or "i don't know" in blooben or "i dont know" in blooben or "huh" in blooben or "?" in blooben:
         winsound.PlaySound("Grain_Says_Please_Hold.wav", 0)
         winsound.PlaySound("Please_Hold.wav", 0)
         newForm()
